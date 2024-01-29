9e1a54594d40a96a7bead1b6f7a58f136aa7d3b0
Erik Martín Garzón <erikmarting1200@gmail.com>
2024-01-29 14:11:40 +0100

    fade out try

--- Nautilus-7a-GameJam/Assets/Scripts/Enviroment/TimerScript.cs
+++ Nautilus-7a-GameJam/Assets/Scripts/Enviroment/TimerScript.cs
@@ -1,12 +1,16 @@
+using Microsoft.Unity.VisualStudio.Editor;
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class TimerScript : MonoBehaviour
 {
 
     public float tiempoLimite = 10.0f;
     private float tiempoRestante;
+
+    public float duracionFadeOut = 2.0f;
+    public UnityEngine.UI.Image pantallaFadeOut;
 
     // Start is called before the first frame update
     void Start()
@@ -34,7 +38,25 @@ public class TimerScript : MonoBehaviour
 
     void ReiniciarNivel()
     {
+        StartCoroutine(FadeOutAndReload());
         UnityEngine.SceneManagement.SceneManager.LoadScene(UnityEngine.SceneManagement.SceneManager.GetActiveScene().name);
 
     }
+
+
+    IEnumerator FadeOutAndReload()
+    {
+        float tiempoInicioFade = Time.time;
+
+        while (Time.time - tiempoInicioFade < duracionFadeOut)
+        {
+            float alpha = Mathf.Lerp(0f, 1f, (Time.time - tiempoInicioFade) / duracionFadeOut);
+            pantallaFadeOut.color = new Color(0f, 0f, 0f, alpha);
+            yield return null;
+        }
+
+        // Recarga la escena después del fade out
+        ReiniciarNivel();
+    }
+
 }
