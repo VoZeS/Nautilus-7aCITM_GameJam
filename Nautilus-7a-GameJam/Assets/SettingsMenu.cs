aaef39f2e1db8e76a66b4dc9aabeda1536ab4141
JuanDeDiosGarcia <61006172+JuanDeDiosGarcia@users.noreply.github.com>
2024-02-01 12:25:43 +0100

    MainMenuScene

--- Nautilus-7a-GameJam/Assets/SettingsMenu.cs
+++ Nautilus-7a-GameJam/Assets/SettingsMenu.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Audio;
 using UnityEngine.UI;
 
 public class SettingsMenu : MonoBehaviour
@@ -8,35 +9,60 @@ public class SettingsMenu : MonoBehaviour
     public Toggle toggleFullScreen;
     public Slider sliderVolumen;
     public AudioListener audioListener;
+    public AudioMixer audioMixer;  // Agrega una referencia al mezclador de audio
+
+    //public string groupName = "NombreDelGrupo";  // Reemplaza con el nombre del grupo en tu mezclador
 
     void Start()
     {
         // Configuración inicial
         toggleFullScreen.isOn = Screen.fullScreen;
-        sliderVolumen.value = AudioListener.volume;
+        sliderVolumen.value = PlayerPrefs.GetFloat("Volumen", 1.0f); // Recupera el volumen guardado (si existe)
+        AdjustBackgroundVolume();
+        AdjustVFXVolume();
     }
 
     public void Update()
     {
-        AdjustVolume();
         ToggleFullScreen();
+        AdjustBackgroundVolume();
+        AdjustVFXVolume();
+
+
     }
 
     public void ToggleFullScreen()
     {
-        if(toggleFullScreen.isOn)
-        {
-            Screen.fullScreen = true;
-        }
-        else
-        {
-            Screen.fullScreen = false;
-        }
-        
+        Screen.fullScreen = toggleFullScreen.isOn;
     }
 
-    public void AdjustVolume()
+    public void AdjustBackgroundVolume()
     {
-        AudioListener.volume = sliderVolumen.value;
+        float volumen = sliderVolumen.value;
+
+        // Guarda el volumen actual en PlayerPrefs
+        PlayerPrefs.SetFloat("Volumen", volumen);
+
+        // Ajusta el parámetro del grupo en el mezclador de audio
+        audioMixer.SetFloat("BackgroundVol", Mathf.Log10(volumen) * 20);
+        if (volumen == 0)
+        {
+            audioMixer.SetFloat("BackgroundVol", Mathf.Log10(0.1f) * 20);
+        }
     }
+    public void AdjustVFXVolume()
+    {
+        float volumen = sliderVolumen.value;
+
+        // Guarda el volumen actual en PlayerPrefs
+        PlayerPrefs.SetFloat("Volumen", volumen);
+
+        // Ajusta el parámetro del grupo en el mezclador de audio
+        audioMixer.SetFloat("VfxVolumen", Mathf.Log10(volumen) * 20);
+        if (volumen == 0)
+        {
+            audioMixer.SetFloat("VfxVolumen", Mathf.Log10(0.1f) * 20);
+        }
+    }
+
 }
