d0e8669fb8c0a64df9f1dee4b1977ee5888cb928
JuanDeDiosGarcia <61006172+JuanDeDiosGarcia@users.noreply.github.com>
2024-02-01 12:40:51 +0100

    Last Fixes

--- Nautilus-7a-GameJam/Assets/SettingsMenu.cs
+++ Nautilus-7a-GameJam/Assets/SettingsMenu.cs
@@ -9,7 +9,7 @@ public class SettingsMenu : MonoBehaviour
     public Toggle toggleFullScreen;
     public Slider sliderVolumen;
     public AudioListener audioListener;
-    public AudioMixer audioMixer;  // Agrega una referencia al mezclador de audio
+    
 
     //public string groupName = "NombreDelGrupo";  // Reemplaza con el nombre del grupo en tu mezclador
 
@@ -18,16 +18,15 @@ public class SettingsMenu : MonoBehaviour
         // Configuración inicial
         toggleFullScreen.isOn = Screen.fullScreen;
         sliderVolumen.value = PlayerPrefs.GetFloat("Volumen", 1.0f); // Recupera el volumen guardado (si existe)
-        AdjustBackgroundVolume();
-        AdjustVFXVolume();
+        AdjustVolume();
+
     }
 
     public void Update()
     {
         ToggleFullScreen();
-        AdjustBackgroundVolume();
-        AdjustVFXVolume();
 
+        AdjustVolume();
 
     }
 
@@ -36,33 +35,10 @@ public class SettingsMenu : MonoBehaviour
         Screen.fullScreen = toggleFullScreen.isOn;
     }
 
-    public void AdjustBackgroundVolume()
+    public void AdjustVolume()
     {
-        float volumen = sliderVolumen.value;
-
-        // Guarda el volumen actual en PlayerPrefs
-        PlayerPrefs.SetFloat("Volumen", volumen);
-
-        // Ajusta el parámetro del grupo en el mezclador de audio
-        audioMixer.SetFloat("BackgroundVol", Mathf.Log10(volumen) * 20);
-        if (volumen == 0)
-        {
-            audioMixer.SetFloat("BackgroundVol", Mathf.Log10(0.1f) * 20);
-        }
+        AudioListener.volume = sliderVolumen.value;
     }
-    public void AdjustVFXVolume()
-    {
-        float volumen = sliderVolumen.value;
-
-        // Guarda el volumen actual en PlayerPrefs
-        PlayerPrefs.SetFloat("Volumen", volumen);
-
-        // Ajusta el parámetro del grupo en el mezclador de audio
-        audioMixer.SetFloat("VfxVolumen", Mathf.Log10(volumen) * 20);
-        if (volumen == 0)
-        {
-            audioMixer.SetFloat("VfxVolumen", Mathf.Log10(0.1f) * 20);
-        }
-    }
+    
 
 }
